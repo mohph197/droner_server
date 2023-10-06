644f9eb92b13dfdfc1c3870174bef3b49a9dbc08
Mohammed Tirichine <mohtiri718@gmail.com>
2023-10-06 14:32:39 +0100

    Schedule config

--- app/main.py
+++ app/main.py
@@ -1,16 +1,23 @@
 import os
 from dotenv import load_dotenv
 from threading import Thread
+import schedule
 
 load_dotenv()
 
 from app.web.fastapi.main import start_fastapi_webserver
-from app.components.uavs.battery_check import check_battery_stats
+from app.components.uavs.battery_check import start_battery_check
 from app.components.real_time_data_capture.mqtt_events_capture import (
-    start_real_time_data_capture,
+    start_real_time_data_capture, stop_real_time_data_capture
 )
 
 thread = Thread(target=start_real_time_data_capture)
-check_battery_stats()
+thread.start()
+start_battery_check()
+
+schedule_thread = Thread(target=schedule.run_pending)
+schedule_thread.start()
 
 app = start_fastapi_webserver()
+
+app.on_event("shutdown")(lambda: stop_real_time_data_capture())
